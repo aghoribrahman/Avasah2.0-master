25b39a3e6f234106d4bd396769528e604f851e4f
Mohit Trivedi <78268728+aghoribrahman@users.noreply.github.com>
2023-05-18 16:06:55 +0530

    light

--- avasah2/avasah2/settings.py
+++ avasah2/avasah2/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_ynogg6ubda8)i(&_52@y5!9z%(8ty$cnf%*tk^+c)b=y&@5mm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 #postgresql://postgres:[Tr7662230066_wcdr]@db.prbhestzyrinxscqzntp.supabase.co:5432/postgres
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 
 
